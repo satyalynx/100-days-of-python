32640b365f55446cac39daee6003b3591af4000c
satyalynx <satyarana384@gmail.com>
2025-09-15 23:03:39 +0530

    Day 6 complete - Functions

---
+++ Day 6/Functions/task.py
@@ -0,0 +1,17 @@
+#functions - a component of code, which has some functionalities.
+
+#defining function - def my_function():
+#calling function - my_function()
+
+print("Hello")
+num_clear = len("Hello")
+print(num_clear)
+
+#Own Function - we need a keyword called "def"
+def my_function():
+    print("Hello")
+    print("Bye") #this result nothing bcoz we haven't executed it yet.
+
+my_function() #Now, the code will run.
+
+#NOTE: for more practice on functions had to Reeborg's World!
