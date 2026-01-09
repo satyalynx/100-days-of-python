a7040c6eb0778c18c9aba18830c4256d8c830e1d
satyalynx <satyarana384@gmail.com>
2026-01-09 20:06:12 +0530

    Day 13 complete - Use of Debugger

--- Day 13/Fix the Errors/task.py
+++ Day 13/Fix the Errors/task.py
@@ -1,3 +1,8 @@
-age = int(input("How old are you?"))
+try:
+    age = int(input("How old are you?"))
+except ValueError:
+    print("You have typed in a invalid number. Please try again with a numerical responds such as 15.")
+    age =int(input("How old are you?"))
+
 if age > 18:
-print("You can drive at age {age}.")
+    print(f"You can drive at age {age}.")
